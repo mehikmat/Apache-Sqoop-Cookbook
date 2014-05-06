c6c36e6b38199844ca66b7787605b53e9f974905
Hikmat Dhamee <mehikmat@users.noreply.github.com>
2014-05-06 09:36:22 -0700

    add `use sqoop` statement
    If I don't use the `use database_name;` statement,tables are being created in default database `mysql`.

--- mysql.tables.sql
+++ mysql.tables.sql
@@ -14,6 +14,9 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+-- Select database sqoop
+USE sqoop;
+
 -- Create table cities
 CREATE TABLE `cities` (
   `id` INTEGER UNSIGNED NOT NULL,
